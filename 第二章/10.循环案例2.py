90437bacb59b6c5505805cdf565551f6748c871a
east-666 <2308878016@qq.com>
2026-09-10 18:47:20 +0800

    9月10日晚列表和循环遗留的练习题

--- 第二章/10.循环案例2.py
+++ 第二章/10.循环案例2.py
@@ -34,4 +34,51 @@
 # print(f"随机生成的数字是：{random_number}。")
 
 
+
+# 练习题目
+
+# # 题目1--根据用户输入的三角形边长，打印直角三角形
+
+#     #用户输入
+# l = int(input("请输入要打印的直角三角形边长："))
+#     #循环打印-----外层循环控制行，内层控制列。
+# for i in range(l):
+#     j = 0
+#     print("*",end=" ")
+#     while j < i:
+#         print("*",end=" ")
+#         j += 1
+#     else:
+#         print()
+
+
+# # 题目2--根据输入的数字，打印数字金字塔
+#     #用户输入
+# l = int(input("请输入要打印的数字金字塔边长："))
+#     #循环打印-----外层循环控制行，内层控制列。
+# for i in range(l):
+#     j = 2
+#     print(1,end=" ")
+#     while j - 2 < i:
+#         print(j,end=" ")
+#         j += 1
+#     else:
+#         print()
+
+
+# # 题目3--打印国际象棋棋盘
+# for i in range(8):
+#     if (i + 1) % 2 == 0:
+#         print("o",end=" ")
+#     else:
+#         print("*",end=" ")
+#     for j in range(7):
+#         if (j + 1) % 2 == 0:
+#             print("*",end=" ")
+#         else:
+#             print("o",end=" ")
+#     print()
+
+
+
     
